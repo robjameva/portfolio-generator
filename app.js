ba14c8d793929713872fe023b757a89d7a14ea26
Robert Evanik <rob.evanik@gmail.com>
2021-12-13 17:56:51 -0500

    Installed Inquirer

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
