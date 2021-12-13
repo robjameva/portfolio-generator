cf84446140145b0622f71d0aaaa7a796c476af2c
Robert Evanik <rob.evanik@gmail.com>
2021-12-12 20:36:46 -0500

    Starting with Node

---
+++ app.js
@@ -0,0 +1,14 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs)
+
+const printProfileData = profileDataArr => {
+    // profileDataArr.forEach((profileItem) => {
+    //     console.log(profileItem)
+    // });
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+
+
+printProfileData(profileDataArgs);
